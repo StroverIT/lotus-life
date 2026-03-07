7baa76cbca50fb484de32f8b766bcb001ea6a3c8
StroverIT <xcreaw1@abv.bg>
2026-03-07 10:16:53 +0200

    Add dynamic rendering to YogaPage by setting the dynamic export to "force-dynamic".

--- src/app/yoga/page.tsx
+++ src/app/yoga/page.tsx
@@ -1,6 +1,8 @@
 import { getSchedule, getEvents } from "@/lib/yogaData";
 import Yoga from "@/pages/Yoga";
 
+export const dynamic = "force-dynamic";
+
 export default async function YogaPage() {
   const [schedule, events] = await Promise.all([getSchedule(), getEvents()]);
   return <Yoga initialSchedule={schedule} initialEvents={events} />;
